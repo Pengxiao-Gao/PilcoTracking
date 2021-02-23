1cb1424c81b65d1546a78b0459a03abf39a1d342
StefanGao1114 <504207850@qq.com>
2021-02-23 17:06:23 +0100

    03

---
+++ test.py
@@ -0,0 +1,12 @@
+
+
+from model.robot_parameter import Robot_Parameter
+from model.ackermann import Ackermann
+
+if __name__ == "__main__":
+    robot_parameter = Robot_Parameter()
+
+    ackermann = Ackermann()
+
+
+
